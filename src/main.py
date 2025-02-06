efe3c23b95d944b9a70a33a47550a8615708b1c4
riddler_rid <unkownrid@gmail.com>
2025-02-06 12:16:11 +0300

    create settings

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
 
+from src.api import router as api_router
+from src.core.config import settings
+
 app = FastAPI()
+app.include_router(api_router, prefix=settings.api.api_prefix)
 
 @app.get("/")
 async def root():
